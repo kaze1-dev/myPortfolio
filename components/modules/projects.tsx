83e4f65d78a6a37b19b2f5b4e1a7150bf95f8ffd
kaze <kaze0001111@gmail.com>
2026-04-05 07:53:02 +0500

    style: project cards

--- components/modules/projects.tsx
+++ components/modules/projects.tsx
@@ -4,43 +4,85 @@ import { Icons } from '../icons'
 const Projects = () => {
   return (
     <section className='h-screen font-rounded tracking-tight px-34'>
-      <h1 className='text-4xl tracking-tight font-bold text-slate-300 mb-10'>/ Projects</h1>
-     <div className='grid grid-cols-3 gap-4'>
-      <div className='bg-slate-800 rounded-lg p-4'>
+      <h1 className='text-4xl tracking-tight font-bold text-slate-300 my-10'>/ Projects</h1>
+     <div className='grid grid-cols-3 gap-2'>
+      <div className='bg-[#112240] rounded-2xl px-8 pt-8 pb-20'>
         <div className='flex justify-between items-center'>
           <Icons.folder className='w-8 h-8' />
           <Icons.gitHub className='w-6 h-6' />
         </div>
+        <h2 className='text-xl font-bold text-slate-300 mt-6'>SaaS subscription<br/> Dashboard</h2>
+        <p className='text-sm text-slate-400 mt-2 leading-relaxed'>
+          A brief description of the project goes here. It highlights the main features and technologies used.
+        </p>
+        <p className='text-sm text-slate-400 mt-10'>
+          TypeScript, Next.js, Prisma, Tailwind CSS
+        </p>
       </div>
-      <div className='bg-slate-800 rounded-lg p-4'>
+      <div className='bg-[#112240] rounded-2xl px-8 pt-8 pb-20'>
         <div className='flex justify-between items-center'>
           <Icons.folder className='w-8 h-8' />
           <Icons.gitHub className='w-6 h-6' />
         </div>
+        <h2 className='text-xl font-bold text-slate-300 mt-6'>Project Title</h2>
+        <p className='text-sm text-slate-400 mt-2 leading-relaxed'>
+          A brief description of the project goes here. It highlights the main features and technologies used.
+        </p>
+        <p className='text-sm text-slate-400 mt-10'>
+          TypeScript, Next.js, Prisma, Tailwind CSS
+        </p>
       </div>
-      <div className='bg-slate-800 rounded-lg p-4'>
+      <div className='bg-[#112240] rounded-2xl px-8 pt-8 pb-20'>
         <div className='flex justify-between items-center'>
           <Icons.folder className='w-8 h-8' />
           <Icons.gitHub className='w-6 h-6' />
         </div>
+        <h2 className='text-xl font-bold text-slate-300 mt-6'>Project Title</h2>
+        <p className='text-sm text-slate-400 mt-2 leading-relaxed'>
+          A brief description of the project goes here. It highlights the main features and technologies used.
+        </p>
+        <p className='text-sm text-slate-400 mt-10'>
+          TypeScript, Next.js, Prisma, Tailwind CSS
+        </p>
       </div>
-      <div className='bg-slate-800 rounded-lg p-4'>
+      <div className='bg-[#112240] rounded-2xl px-8 pt-8 pb-20'>
         <div className='flex justify-between items-center'>
           <Icons.folder className='w-8 h-8' />
           <Icons.gitHub className='w-6 h-6' />
         </div>
+        <h2 className='text-xl font-bold text-slate-300 mt-6'>Project Title</h2>
+        <p className='text-sm text-slate-400 mt-2 leading-relaxed'>
+          A brief description of the project goes here. It highlights the main features and technologies used.
+        </p>
+        <p className='text-sm text-slate-400 mt-10'>
+          TypeScript, Next.js, Prisma, Tailwind CSS
+        </p>
       </div>
-      <div className='bg-slate-800 rounded-lg p-4'>
+      <div className='bg-[#112240] rounded-2xl px-8 pt-8 pb-20'>
         <div className='flex justify-between items-center'>
           <Icons.folder className='w-8 h-8' />
           <Icons.gitHub className='w-6 h-6' />
         </div>
+        <h2 className='text-xl font-bold text-slate-300 mt-6'>Project Title</h2>
+        <p className='text-sm text-slate-400 mt-2 leading-relaxed'>
+          A brief description of the project goes here. It highlights the main features and technologies used.
+        </p>
+        <p className='text-sm text-slate-400 mt-10'>
+          TypeScript, Next.js, Prisma, Tailwind CSS
+        </p>
       </div>
-      <div className='bg-slate-800 rounded-lg p-4'>
+      <div className='bg-[#112240] rounded-2xl px-8 pt-8 pb-20'>
         <div className='flex justify-between items-center'>
           <Icons.folder className='w-8 h-8' />
           <Icons.gitHub className='w-6 h-6' />
         </div>
+        <h2 className='text-xl font-bold text-slate-300 mt-6'>Project Title</h2>
+        <p className='text-sm text-slate-400 mt-2 leading-relaxed'>
+          A brief description of the project goes here. It highlights the main features and technologies used.
+        </p>
+        <p className='text-sm text-slate-400 mt-10'>
+          TypeScript, Next.js, Prisma, Tailwind CSS
+        </p>
       </div>
      </div>
     </section>
